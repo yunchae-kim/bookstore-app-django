3d1fb68b1fc90d5b748283a1db63b74f6210dd10
yunchae-kim <yunchae.dev@gmail.com>
2024-02-16 12:43:45 -0500

    fix: add media settings for file uploads
    - Added `MEDIA` settings to `settings.py` to configure file upload
    functionality.

--- bookstore_project/bookstore_project/settings.py
+++ bookstore_project/bookstore_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -135,3 +136,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
+
+# Media settings for file uploads
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
