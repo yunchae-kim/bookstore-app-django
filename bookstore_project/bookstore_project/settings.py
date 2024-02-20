840e1ef4a2f20a76c0e122380550269503827125
yunchae-kim <yunchae.dev@gmail.com>
2024-02-20 09:26:37 -0500

    feat: add content type based response
    - Updated `settings.py` to implement REST-API returning JSON or XML based on `Content-Type` header.
    - Updated `requirements.txt` to add `djangorestframework-xml`.

--- bookstore_project/bookstore_project/settings.py
+++ bookstore_project/bookstore_project/settings.py
@@ -138,6 +138,14 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework_xml.renderers.XMLRenderer",
+    ),
+    "DEFAULT_PARSER_CLASSES": (
+        "rest_framework.parsers.JSONParser",
+        "rest_framework_xml.parsers.XMLParser",
+    ),
 }
 
 # Media settings for file uploads
